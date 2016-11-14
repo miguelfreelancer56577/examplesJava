e986a4ac1ee768bcdc1bda4148f2bce08aef154a
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2016-11-14 08:40:39 -0600

    how create a new file

--- src/main/java/practice/Test.java
+++ src/main/java/practice/Test.java
@@ -1,16 +1,18 @@
 package practice;
 
-import org.springframework.web.client.RestTemplate;
-
-import practice.model.User;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// TODO how create a file
+		File test = new File("test.txt");
+		if(test.createNewFile()){
+			System.out.println("File Create");
+		}else{
+			assert(false):"Error to create file, or already exist";
+		}
 		
 	}
 
