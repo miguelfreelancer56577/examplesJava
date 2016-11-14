e5c25747a89fabbb9fe71a960ad621b71569a677
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2016-11-14 08:08:41 -0600

    Clean the main method.

--- src/main/java/practice/Test.java
+++ src/main/java/practice/Test.java
@@ -11,19 +11,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ObjectMapper mp = new ObjectMapper();
-		User user = new User("Miguel", "Torres");
-		user.setName("Luis");
-		user.setSurname("Martinez");
-		try {
-			System.out.println(mp.writeValueAsString(user));
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally{
-			System.out.println("block finally ...");
-		}
-		System.out.println("ended the application ...");
+		
 	}
 
 }
