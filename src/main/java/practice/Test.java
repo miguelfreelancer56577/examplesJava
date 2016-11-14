94cce949d286f70dada78a388ff1f26af097ed26
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2016-11-14 09:53:16 -0600

    How write and read a obeject file

--- src/main/java/practice/Test.java
+++ src/main/java/practice/Test.java
@@ -1,44 +1,32 @@
 package practice;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.Scanner;
 
+import practice.model.User;
+
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		// TODO how create a file
-		File test = new File("test.txt");
-		if(test.exists()){
-			System.out.println("Absolute path: " + test.getAbsolutePath());
-			Scanner in = new Scanner(test).useDelimiter("\n");
-			int cont = 0;
-			while (in.hasNext()) {
-				++cont;
-				System.out.println("Record number " + cont + " : " + in.next());
-			}
+		// TODO how write a object in a new file
+		File in = new File("people.obj");
+		if(in.exists()){
+			FileOutputStream file = new FileOutputStream(in);
+			ObjectOutputStream people = new ObjectOutputStream(file);
+			people.writeObject(new User("Miguel", "Torres"));
+			people.writeObject(new User("Eduardo", "Torres"));
+			people.writeObject(new User("Marcos", "Torres"));
+			people.writeObject(new User("Maria", "Torres"));
+			people.writeObject(new User("Angela", "Torres"));
+			people.close();
 		}else{
-			if(test.createNewFile()){
-				System.out.println("File Create");
-				//names
-				String names[] = {"Miguel", "Luis" , "Pedro", "Carlos", "Pepe", "Raul", "Manuel", "Carlitos"};
-//				create object of output
-				PrintWriter out = new PrintWriter(test);
-				// Populate a new file
-				for (int i = 0; i < names.length; i++) {
-					out.println(names[i]);
-				}
-				out.flush();
-				out.close();
-				// re-run  an application
-				main(null);
-			}else{
-				assert(false):"Error to create file, or already exist";
-			}
+			assert(false):"Error to create file, or already exist";
 		}
-		
 		
 	}
 
