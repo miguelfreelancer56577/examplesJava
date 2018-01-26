e59fa14f43ec808dcf94e43d8f66845681d04161
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2018-01-26 16:48:58 -0600

    question 14

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,29 @@
 import java.io.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.Comparator;
+import java.util.Set;
 
 public class App {
 	
 
 	public static void main(String[] args) {
 
-		
-		
-	}
-	
-	public void takeList(List<? extends String> list) {
-		
-		list.add("foo");
-		Object o = list;
-		String s = list.get(0);
-		list = new ArrayList<String>();
-		list = new ArrayList<Object>();
+		Set<Integer> set = new HashSet<Integer>();
+		Integer i1 = 45;
+		Integer i2 = 46;
+		set.add(i1);
+		set.add(i1);
+		set.add(i2); 
+		System.out.print(set.size() + " ");
+		set.remove(i1); 
+		System.out.print(set.size() + " ");
+		i2 = 47;
+		set.remove(i2); 
+		System.out.print(set.size() + " ");
 		
 	}
 
