ac493555ff41f6bec8615133374b5ccb7a3b580c
miguelfreelancer56577 <miguelfreelancer56577@gmail.com>
2018-01-26 17:28:50 -0600

    question 15_3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,22 +13,6 @@ public class App {
 
 	public static void main(String[] args) {
 
-
-		TreeSet<String> s = new TreeSet<>();
-		TreeSet<String> subs = new TreeSet<>();
-		
-		for(int i = 806; i < 813; i++)
-			if(i%2 == 0) s.add("a"+i);
-		
-		for(int i = 606; i < 613; i++)
-			if(i%2 == 0) s.add("a"+i);
-		
-		subs = (TreeSet)s.subSet("a1", true, "a4", true);
-		
-		s.add("a"+1);
-		
-		System.out.println(s + " " + subs);
-		
 		TreeSet<String> authors = new TreeSet<String>();
 		
 	    authors.add("Andreas Gryphius");
@@ -42,8 +26,19 @@ public class App {
 	    authors.add("Milla Jovovich");
 	    authors.add("Taras Schewtschenko");
 	    
-	    System.out.println(authors.subSet("Milla", "Wladimir"));
-		
+	    System.out.println(authors.first());
+	    System.out.println(authors.floor("F"));
+	    System.out.println(authors.contains("A"));
+	    System.out.println(authors.higher("A"));
+	    System.out.println(authors.pollFirst());
+	    System.out.println(authors.pollLast());
+	    
+	    Object[] arrayAuthors = authors.toArray();
+	    
+	    for (Object object : arrayAuthors) {
+			System.out.println("name author: " + object);
+		}
+	    
 	}
 
 }
